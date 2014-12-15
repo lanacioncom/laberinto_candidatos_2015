2aaf402b1800f7ef2a694b7389ff2c892b7f2f51
Gaston de la llana <gasdela@gmail.com>
2014-12-15 12:50:05 -0300

    subida

---
+++ data/candidatos.js
@@ -0,0 +1,113 @@
+var canDatos = [
+
+    candidato1 = { 
+        nombre:"Daniel Scioli",
+        partido:"FPV",
+        twitter:"@danielscioli",
+        foto:"img/cand-gr/scioli.jpg",
+        edad:"58 Años",
+        profesion:"Deportista",
+        web:"http://www.danielscioli.com.ar",
+        link:"danielscioli.com.ar",
+        bio:"Pasó su infancia en Villa Crespo, cursó la secundaria en el colegio Carlos Pellegrini de la UBA y estudió Marketing en la UADE. Fue deportista y empresario antes de ingresar en la política."
+    },
+    
+    candidato2 = { 
+        nombre:"Sergio Massa",
+        partido:"Frente Renovador",
+        twitter:"@SergioMassa",
+        foto:"img/cand-gr/massa.jpg",
+        edad:"42 Años",
+        profesion:"Abogado",
+        web:"http://www.sergiomassa.org",
+        link:"sergiomassa.org",
+        bio:"Nació en San Martín y cursó la secundaria en el Colegio Agustiniano, donde militó en la Ucedé.  Se recibió de abogado en 2013 en la Universidad de Belgrano, ya instalado en la política."
+    },
+
+    candidato3 = { 
+        nombre:"Mauricio Macri",
+        partido:"PRO",
+        twitter:"@mauriciomacri",
+        foto:"img/cand-gr/macri.jpg",
+        edad:"55 Años",
+        profesion:"Ingeniero",
+        web:"http://www.mauriciomacri.com.ar",
+        link:"mauriciomacri.com.ar",
+        bio:"Nacido en Tandil, se educó en el Colegio Cardenal Newman y se recibió de ingeniero en Universidad Católica. Fue presidente del club Boca Juniors y empresario antes de incursionar en política."
+    },
+
+    candidato4 = { 
+        nombre:"Florencio Randazzo",
+        partido:"FPV",
+        twitter:"@RandazzoF",
+        foto:"img/cand-gr/randazzo.jpg",
+        edad:"50 Años",
+        profesion:"Contador público",
+        web:"http://www.florenciorandazzo.com.ar",
+        link:"florenciorandazzo.com.ar",
+        bio:"Oriundo de Chivilcoy, realizó sus estudios secundarios en la ENET Nº 1 Mariano Moreno, donde comenzó a militar en política. Se graduó como contador público en la UBA."
+    },
+
+    candidato5 = { 
+        nombre:"Julio Cobos",
+        partido:"UNEN",
+        twitter:"@juliocobos",
+        foto:"img/cand-gr/cobos.jpg",
+        edad:"59 Años",
+        profesion:"Ingeniero",
+        web:"#",
+        link:"No posee",
+        bio:"Nació en la ciudad de Godoy Cruz, Mendoza. Cursó la secundaria en el Liceo Militar General Espejo y se graduó en la UTN de Mendoza, donde tiempo después fue decano."
+    },
+
+    candidato6 = { 
+        nombre:"Hermes Binner",
+        partido:"UNEN",
+        twitter:"@HermesBinner",
+        foto:"img/cand-gr/binner.jpg",
+        edad:"71 Años",
+        profesion:"Médico",
+        web:"http://www.hermesbinner.com.ar",
+        link:"hermesbinner.com.ar",
+        bio:"Nació en Rafaela, Santa Fe. Desde su adolescencia militó en el socialismo. Se graduó en la Universidad Nacional de Rosario como médico, especializado en anestesiología."
+    },
+
+    candidato7 = { 
+        nombre:"Jorge Altamira",
+        partido:"Fte. de Izq. y de los Trabajadores",
+        twitter:"@altamirajorge",
+        foto:"img/cand-gr/altamira.jpg",
+        edad:"72 Años",
+        profesion:"Periodista",
+        web:"#",
+        link:"No posee",
+        bio:"Nacido en Buenos Aires, participó desde joven en huelgas obreras. Se llama Saúl José Wermus y Altamira fue el seudónimo que adoptó para escribir en los años de dictadura."
+    },
+
+    candidato8 = { 
+        nombre:"Elisa Carrió",
+        partido:"Coalición Cívica (alejada de la mesa de UNEN)",
+        twitter:"@elisacarrio",
+        foto:"img/cand-gr/carrio.jpg",
+        edad:"57 Años",
+        profesion:"Abogada",
+        web:"http://www.elisacarrio.com.ar",
+        link:"elisacarrio.com.ar",
+        bio:"Nació y creció en Chaco. Se recibió de abogada en la Universidad Nacional del Nordeste. Trabajó brevemente en la justicia y luego inició su carrera política desde el radicalismo."
+    },
+
+
+    candidato9 = { 
+        nombre:"Ernesto Sanz",
+        partido:"UNEN",
+        twitter:"@SanzErnesto",
+        foto:"img/cand-gr/sanz.jpg",
+        edad:"57 Años Años",
+        profesion:"Abogado",
+        web:"http://www.ernestosanz.org.ar",
+        link:"ernestosanz.org.ar",
+        bio:"Nació y creció en Chaco. Se recibió de abogada en la Universidad Nacional del Nordeste. Trabajó brevemente en la justicia y luego inició su carrera política desde el radicalismo."
+    },
+
+    
+]
